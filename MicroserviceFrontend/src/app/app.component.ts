443b3ab42f8f0d74647250d6960aff98d5dd3e7b
Doom <indianowski@gmail.com>
2025-05-15 22:08:02 +0200

    small css changes

--- MicroserviceFrontend/src/app/app.component.ts
+++ MicroserviceFrontend/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {EntryChoiceListComponent} from './entry-choice-list/entry-choice-list.component';
-import {KeycloakService} from 'keycloak-angular';
-import {NgIf} from '@angular/common';
-import {environment} from '../environments/environment.development';
 import {MenuComponent} from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EntryChoiceListComponent, MenuComponent],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
